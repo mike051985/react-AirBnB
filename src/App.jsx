1c251903a737bcc5f3f9fac23ec547cef74c8cef
Michelet Saint-Preux <michelet2000@gmail.com>
2024-02-26 11:42:56 +0800

    index.html src/App.jsx src/components/Card.jsx src/components/Hero.jsx src/components/Navbar.jsx src/images/airbnb-logo.png src/images/katie-zaferes.png src/images/photo-grid.png src/images/star.png

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Card from './components/Card'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <Hero />
+      <Card />
+    </div>
   )
 }
