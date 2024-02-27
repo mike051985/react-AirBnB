4be9b71f377a250c8cc29e6ff10a09475da2bc9a
Michelet Saint-Preux <michelet2000@gmail.com>
2024-02-27 12:38:16 +0800

    src/App.jsx src/Data.jsx src/components/Card.jsx src/images/mountain-bike.png src/images/wedding-photography.png

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,28 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import Data from './Data'
 
 export default function App() {
+
+  const cards = Data.map(item => {
+    return (
+      <Card 
+          imgUrl={item.coverImg}
+          rating={item.stats.rating}
+          reviewCount={item.stats.reviewCount}
+          location={item.location}
+          title={item.title}
+          price={item.price}
+      />
+    )
+  })
+
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+        {cards}
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import ImageCard from '../images/katie-zaferes.png'
+//import ImageCard from '../images/katie-zaferes.png'
 import ImageStats from '../images/star.png'
 import '../App.css'
 
-export default function Card() {
+export default function Card(props) {
+  console.log(props)
   return (
     <div className='card'>
-      <img src={ImageCard} className='card--image' />
+      <img src={props.imgUrl} className='card--image' />
       <div className='card--stats'>
         <img src={ImageStats} className='card--star' />
-        <span>5.0</span>
-        <span className='gray'>(6) . </span>
-        <span className='gray'>USA</span>
+        <span>{props.rating}</span>
+        <span className='gray'>({props.reviewCount}) . </span>
+        <span className='gray'>{props.location}</span>
       </div>
-      <h2>Life lessons with Katie Zaferes</h2>
-      <p><span className='bold'>From $136 </span>/ person</p>
+      <h2>{props.title}</h2>
+      <p><span className='bold'>From ${props.price} </span>/ person</p>
     </div>
   )
 }
